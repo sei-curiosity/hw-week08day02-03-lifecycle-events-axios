ff719cab9f526632ef36ec7c4bcf2831a1e11b23
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-06 23:43:37 +0300

    After copy

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lyfcycle from './components/Lyfcycle.jsx'
+import Header from './components/Header.jsx'
+import Memegen from './components/Memegen.jsx'
 
 function App() {
   return (
     <div className="App">
-
+      <Lyfcycle />
+      <Header />
+      <Memegen /> 
     </div>
   );
 }
