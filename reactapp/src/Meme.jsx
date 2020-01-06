909daf1d27f0c7fe7eeabef5fabbe5bd268dad06
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-06 23:41:56 +0300

    before copy

---
+++ reactapp/src/Meme.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Meme extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            mArr = memes
+         }
+    }
+    render( ) { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default Meme;
