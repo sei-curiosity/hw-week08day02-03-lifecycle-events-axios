5c64dbf3a332b88dfff130590f608df1f4d507bf
ahmedalnemer <ahmedalnemer3@gmail.com>
2020-01-07 08:53:57 +0300

    Started working but didn't finish

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Memegen from './Components/Memegen/Memegen';
+
+function App() {
+  return (
+    <div className="App">
+      <Memegen />
+    </div>
+  );
+}
+
+export default App;
