eb85ebd8d8e4f72cec939a36c4f1037fac4fc2dc
monano <mona.gaihab@gmail.com>
2020-01-07 08:38:53 +0300

    Solution for Week08Day03

---
+++ hw8d2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Memes from './components/memes/Memes'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Memes />
+    </div>
+  );
+}
+
+export default App;
