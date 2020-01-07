6104ef544dd7eb0b050629b6eaec75b01d6edec7
monano <mona.gaihab@gmail.com>
2020-01-07 21:54:52 +0300

    Solution for Week08Day03

--- hw8d2/src/App.js
+++ hw8d2/src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import Memes from './components/memes/Memes'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Memes />
-    </div>
-  );
-}
-
+class App extends React.Component {
+  state={
+    pageTheme: false
+  }
+  handlePageThemeclick = () => {
+    console.log(this.state.pageTheme)
+    const preState = !this.state.pageTheme
+    this.setState({pageTheme: preState})
+  }
+  render(){
+    const themeStatus = this.state.pageTheme ? "Memetheme" : ""
+    console.log(themeStatus)
+    return (
+          <div className = {themeStatus}>
+            <button onClick = {this.handlePageThemeclick} >Change Theme</button>
+            <Memes />
+          </div>
+      );
+    }
+  }
 export default App;
--- hw8d2/src/components/memes/Memes.jsx
+++ hw8d2/src/components/memes/Memes.jsx
@@ -1,228 +1,120 @@
 import React from 'react'
+import axios from 'axios'
 import './Memes.css'
 
 class Memes extends React.Component{
     constructor(props){
         super(props)
+        
         this.state ={
             allMemes: [],
+            meme: {
+                name:"",
+                url:"",
+                width:"400",
+                height:"400",
+                id: ""
+            },
+            memeId: "",
             imgStatus: false,
+            topText:"",
+            bottomText:"",
+            randomImg:"https://i.imgflip.com/345v97.jpg"
         }
     }
+    
     componentDidMount(){
-        const allMemes = [
-            {
-            "id": 1,
-            "title": "Angry",
-            "image": "http://imgur.com/kH6o0mN.png"
-            },
-            {
-            "id": 2,
-            "title": "Aww Yeah",
-            "image": "http://imgur.com/vTSZ6CG.png"
-            },
-            {
-            "id": 3,
-            "title": "Cereal Guy",
-            "image": "http://imgur.com/SjnMMhd.png"
-            },
-            {
-            "id": 4,
-            "title": "Cereal Spitting",
-            "image": "http://imgur.com/rdw32yx.png"
-            },
-            {
-            "id": 5,
-            "title": "Challenge Accepted",
-            "image": "http://imgur.com/xHcS2LF.png"
-            },
-            {
-            "id": 6,
-            "title": "Heck Yea",
-            "image": "http://imgur.com/DRd0yKE.png"
-            },
-            {
-            "id": 7,
-            "title": "Concentrated",
-            "image": "http://imgur.com/t9uwvai.png"
-            },
-            {
-            "id": 8,
-            "title": "Derrrp",
-            "image": "http://imgur.com/kBSZgbG.png"
-            },
-            {
-            "id": 9,
-            "title": "Determined",
-            "image": "http://imgur.com/CATjyHj.png"
-            },
-            {
-            "id": 10,
-            "title": "Dude Come On",
-            "image": "http://imgur.com/8Tkr6ld.png"
-            },
-            {
-            "id": 11,
-            "title": "EWBTE",
-            "image": "http://imgur.com/ZaSaYAK.png"
-            },
-            {
-            "id": 12,
-            "title": "Excited Troll",
-            "image": "http://imgur.com/CFLPW9U.png"
-            },
-            {
-            "id": 13,
-            "title": "Fap Fap",
-            "image": "http://imgur.com/NImikb7.png"
-            },
-            {
-            "id": 14,
-            "title": "Forever Alone",
-            "image": "http://imgur.com/4L89AMP.png"
-            },
-            {
-            "id": 15,
-            "title": "Forever Alone Excited",
-            "image": "http://imgur.com/NyEoz2g.png"
-            },
-            {
-            "id": 16,
-            "title": "Oh Yea Female",
-            "image": "http://imgur.com/WMgvwaq.png"
-            },
-            {
-            "id": 17,
-            "title": "Full Panel",
-            "image": "http://imgur.com/iN5P0r4.png"
-            },
-            {
-            "id": 18,
-            "title": "Happy",
-            "image": "http://imgur.com/JqhcMU4.png"
-            },
-            {
-            "id": 19,
-            "title": "Hehehe",
-            "image": "http://imgur.com/U9GK6jL.png"
-            },
-            {
-            "id": 20,
-            "title": "Laughing",
-            "image": "http://imgur.com/wJUtNXR.png"
-            },
-            {
-            "id": 21,
-            "title": "Me Gusta",
-            "image": "http://imgur.com/6otWkQZ.png"
-            },
-            {
-            "id": 22,
-            "title": "Milk",
-            "image": "http://imgur.com/bxMhEKZ.png"
-            },
-            {
-            "id": 23,
-            "title": "Newspaper Guy",
-            "image": "http://imgur.com/MhbPTSQ.png"
-            },
-            {
-            "id": 24,
-            "title": "Okay",
-            "image": "http://imgur.com/X6niVYQ.png"
-            },
-            {
-            "id": 25,
-            "title": "Original Rage",
-            "image": "http://imgur.com/CD0nLh8.png"
-            },
-            {
-            "id": 26,
-            "title": "Original Troll",
-            "image": "http://imgur.com/sV4AReH.png"
-            },
-            {
-            "id": 27,
-            "title": "Poker Face",
-            "image": "http://imgur.com/EFHsGkJ.png"
-            },
-            {
-            "id": 28,
-            "title": "Prrrr",
-            "image": "http://imgur.com/gXmsig6.png"
-            },
-            {
-            "id": 29,
-            "title": "Smile",
-            "image": "http://imgur.com/Bp2y4zT.png"
-            },
-            {
-            "id": 30,
-            "title": "Thoughtful",
-            "image": "http://imgur.com/HdmBrwX.png"
-            },
-            {
-            "id": 31,
-            "title": "Troll Dad Full",
-            "image": "http://imgur.com/Mgyz87x.png"
-            },
-            {
-            "id": 32,
-            "title": "Troll Dad Jump",
-            "image": "http://imgur.com/PFzOfO7.png"
-            },
-            {
-            "id": 33,
-            "title": "Wait A Minute",
-            "image": "http://imgur.com/rxlv8TX.png"
-            },
-            {
-            "id": 34,
-            "title": "What",
-            "image": "http://imgur.com/0JmUzko.png"
-            },
-            {
-            "id": 35,
-            "title": "Why With Hands",
-            "image": "http://imgur.com/gEZW60M.png"
-            },
-            {
-            "id": 36,
-            "title": "YUNO",
-            "image": "http://imgur.com/uXKy3RJ.png"
-            }
-          ] 
-          this.setState(({...preState})=>{
-            preState.allMemes =allMemes
-              return preState
-          })
+        axios.get(`https://api.imgflip.com/get_memes`)
+        .then(res => {const allMemes = res.data.data.memes
+            this.setState({allMemes})
+            
+        })
     }
-    handleMemeTitleClick = () => {
+    handleMemeTitleClick = (memeId) => {
             this.setState(({...preState})=>{
-                // preState.memeId = memeId
                 preState.imgStatus = !this.state.imgStatus
+                preState.memeId = memeId;
+                preState.imgStatus = !this.state.imgStatus;
                 return preState
        })    
     }
+    deleteAllMemes = () => {
+        let allMemes = this.state.allMemes.slice()
+        allMemes =[]
+        this.setState({allMemes})
+    }
+    deleteThisMeme = (memeId) => {
+        let allMemes = this.state.allMemes.slice()
+        allMemes = allMemes.filter(meme => meme.id !== memeId)
+        this.setState({allMemes})
+    }
+    handleSaveChange = (e) => {
+        const allMemes = this.state.allMemes.slice()
+        const memeKey = e.target.name
+        const memeVal = e.target.value
+        const meme = {...this.state.meme}
+        const ID = allMemes.length !== 0 ?(allMemes[allMemes.length-1].id) + 1 : '1' 
+        meme.id = ID
+        meme[memeKey] = memeVal
+        this.setState({meme})
+    }
+    handleSaveSubmit = (e) => {
+        e.preventDefault()
+        const meme = this.state.meme
+        const allMemes = this.state.allMemes.slice()
+        allMemes.push(meme)
+        this.setState({allMemes})
+    }
+    handleWriteChange = (e) => {
+        const {name,value} = e.target
+        // here we put the var to become a key for state
+        this.setState({ [name]: value})
+    }
+    handleWriteSubmit = (e) => {
+        e.preventDefault()
+        const random = Math.floor(Math.random()* this.state.allMemes.length)
+        
+        const randMemeImg = this.state.allMemes[random].url  
+        this.setState(({...preState}) => {
+            preState.randomImg = randMemeImg
+            return preState 
+        })
+       
+    }
     render(){
-        const imgStatus = this.state.imgStatus ? "show" : "hide"
+       
         return(
+            
             <div>
-                <form>
-                    <input type='text' name='title' />
-                    <input type='text' name='image' />
-                    <button>Save Memes</button>
+                <form onSubmit={this.handleSaveSubmit}>
+                    <input type='text' name='name' placeholder="Enter meme title" value={this.state.meme.name} onChange={this.handleSaveChange}/>
+                    <input type='text' name='url' placeholder="Enter meme url" value={this.state.meme.url} onChange={this.handleSaveChange}/>
+                    <button type="submit">Save</button>
                 </form>
+                <form onSubmit={this.handleWriteSubmit}>
+                    <input type="text" name="topText" placeholder="Enter Top Text" value={this.state.topText} onChange={this.handleWriteChange} />
+                    <input type="text" name="bottomText" placeholder="Enter Bottom Text" value={this.state.bottomText} onChange={this.handleWriteChange} />
+                    <button type="submit">Generate</button>
+                </form>
+                <div className="meme">
+                    <img src={this.state.randomImg} alt="meme"/> 
+                    <h2 className="top">{this.state.topText}</h2>
+                     <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
+                {/* <button onClick={this.deleteAllMemes}>Delete All Memes</button>
                  {this.state.allMemes.map(meme => { return (
-                    
-                    <div>
-                     <h1 key={meme.id} onClick={this.handleMemeTitleClick}>{meme.title}</h1>
-                     <div className={imgStatus} id={meme.id}>
-                     <img src={meme.image} alt={meme.title} />
-                     </div>
-                 </div> )})}
-
-                
+                        <div key={meme.id}>
+                        
+                            <h1 onClick={()=>this.handleMemeTitleClick(meme.id)}>{meme.name}</h1>
+                            <button onClick={()=>this.deleteThisMeme(meme.id)}>delete</button>
+                            {meme.id === this.state.memeId && this.state.imgStatus ? 
+                            <img src={meme.url} alt={meme.name} width={meme.width} height={meme.height} /> : null }
+                            <br/>
+                            
+                            <br/><hr/>
+                        </div>
+                    )}) 
+                    }    */}
             </div>
         )
     }
