eb9b92043d3f201e65cf4cb5776e4063af18198a
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-07 23:38:58 +0300

    added state and data and initialized state using componentDidMount

--- lifecycle-axios/src/App.js
+++ lifecycle-axios/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import data from "./data";
 
 class App extends Component {
+  state = {
+    memes: []
+  };
+
+  componentDidMount() {
+    this.setState({ memes: data });
+  }
+
   render() {
     return (
       <div className="App">
