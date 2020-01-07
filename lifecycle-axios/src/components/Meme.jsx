fd7b6b4ad820371954894922450f81bfa8b3bb83
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-07 23:53:17 +0300

    finished 2nd section: Hide/Show Meme

--- lifecycle-axios/src/components/Meme.jsx
+++ lifecycle-axios/src/components/Meme.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
 class Meme extends Component {
+  state = {
+    visible: false
+  };
+
+  handleHide = () => {
+    const { visible } = this.state;
+    this.setState({ visible: !visible });
+  };
+
   render() {
     const { title, image } = this.props;
+
+    // check index.css
+    const className = this.state.visible ? "visible" : "hidden";
+
     return (
       <div className="meme">
-        <h3>{title}</h3>
-        <img src={image} alt={title} />
+        <h3 className="clickable" onClick={this.handleHide}>
+          {title}
+        </h3>
+        <img src={image} alt={title} className={className} />
       </div>
     );
   }
