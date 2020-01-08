1de435532fc668c8f4b321f45f4e4951aa5a72fd
ahmedalnemer <ahmedalnemer3@gmail.com>
2020-01-08 08:53:26 +0300

    HW

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import memes from './memeArray'
 import Memegen from './Components/Memegen/Memegen';
 
-function App() {
-  return (
-    <div className="App">
-      <Memegen />
-    </div>
-  );
+class App extends Component {
+  state = { 
+    memes: memes
+   }
+  render(){
+
+    const allMemes = this.state.memes.map(meme => {
+      return(
+        <div className="meme">
+          <h2>{meme.title}</h2>
+          <img src={meme.image} />
+        </div>
+      )
+    })
+
+    return (
+      <div className="App">
+        {allMemes}
+      </div>
+    )
+  }
 }
 
 export default App;
