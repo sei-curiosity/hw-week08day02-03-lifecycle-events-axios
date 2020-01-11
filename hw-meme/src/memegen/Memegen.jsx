28da1d6e1733f8a9147c5a1d4b796d8393893f3c
Mashael <mashael@Mashaels-MacBook-Pro.local>
2020-01-11 21:45:27 +0300

    solved

--- hw-meme/src/memegen/Memegen.jsx
+++ hw-meme/src/memegen/Memegen.jsx
@@ -3,16 +3,22 @@ import memesData from '../data/memesData';
 import Meme from '../components/Meme'
 class Memegen extends Component {
   state = { 
-            topText: "",
-            bottomText: "",
+            formImage: {
+                title: "",
+                image: ""
+            },
+            
             randomImg: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: []
          }
     
     handleChange = event => {
-        const {name, value} = event.target
-        this.setState({[name]:value})
-        console.log(name)
+        const key = event.target.name
+        const value = event.target.value
+        this.setState( ({...copyState}) =>{
+            copyState.formImage[key] = value
+            return copyState
+        } )
     }
     deleteMeme = (id) => {
         this.setState (({... copyState})=> {
@@ -34,9 +40,22 @@ class Memegen extends Component {
 
     handleSubmit = event => {
         event.preventDefault()
-        const randNum = Math.floor(Math.random()*this.state.allMemeImgs.length)
-        const randMemeImg = this.state.allMemeImgs[randNum].image
-        this.setState({randomImg: randMemeImg}) 
+        const image = {
+            title: this.state.formImage.title,
+            image: this.state.formImage.image,
+            id: this.state.allMemeImgs.length + 1
+        }
+        console.log(image)
+
+        this.setState (({... copyState})=> {
+            copyState.allMemeImgs.push(image)
+            copyState.formImage = {
+                title: "",
+                image: ""
+            }
+            return copyState
+        })
+       
     }
     deleteAllMeme = () => {
         this.setState( ({...copyState}) => {
@@ -50,26 +69,24 @@ class Memegen extends Component {
 
             <div>
                 <button onClick={this.deleteAllMeme}>delete all meme</button>
-                <form className="meme-form" onSubmit={this.handleSubmit}>
+                <form  onSubmit={this.handleSubmit}>
+                    <label>Title</label>
+                    <input name="title" 
+                    value={this.state.formImage.title} onChange={this.handleChange} />
+                    <br />
+                    <label>url</label>
+                    <input name="image" 
+                    value={this.state.formImage.image} onChange={this.handleChange} />
 
-                    <input type="Text" name="topText" placeholder="Top Text" 
-                    value={this.state.topText} onChange={this.handleChange} />
+                    <br />
 
-                    <input type="Text" name="bottomText" placeholder="Bottom Text" 
-                    value={this.state.bottomText} onChange={this.handleChange} />
-
-                    <button>Generate</button>
+                    <input type="submit"/>
                 </form>
                 ${this.state.allMemeImgs.map(img => {
                    return (
                     <Meme image={img.image} title={img.title} delete={this.deleteMeme} id={img.id}/>
                    ) 
                 })}
-                <div className="meme">
-                    <img src={this.state.randomImg} alt="" width="400" height="400" />
-                    <h2 className="top">{this.state.topText}</h2>
-                    <h2 className="bottom">{this.state.bottomText}</h2>
-                </div>
                 
             </div>
          );
